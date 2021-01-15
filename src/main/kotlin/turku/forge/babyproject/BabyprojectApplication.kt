9e62ad264ffcc58a91f803fcb49a2bfa984e7dc7
Dusan Turajlic <dusan@turajlic.fi>
2021-01-16 00:08:26 +0200

    (baby-project) inital commit

---
+++ src/main/kotlin/turku/forge/babyproject/BabyprojectApplication.kt
@@ -0,0 +1,11 @@
+package turku.forge.babyproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BabyprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<BabyprojectApplication>(*args)
+}
---
+++ src/test/kotlin/turku/forge/babyproject/BabyprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package turku.forge.babyproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BabyprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
