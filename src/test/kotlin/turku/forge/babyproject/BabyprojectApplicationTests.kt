0051f1a3573d9339f72c942f9081e2013f85f212
Dusan Turajlic <dusan@turajlic.fi>
2021-01-17 18:21:39 +0200

    (baby-project) adds inital test so the ci can run something

--- src/test/kotlin/turku/forge/babyproject/BabyprojectApplicationTests.kt
+++ src/test/kotlin/turku/forge/babyproject/BabyprojectApplicationTests.kt
@@ -1,5 +1,6 @@
 package turku.forge.babyproject
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,7 @@ class BabyProjectApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		assertThat( true ).isTrue
 	}
 
 }
