b910c3aa1575ed7cc4d963aad8b8c0e102ab8c0a
Dusan Turajlic <dusan@turajlic.fi>
2021-03-18 22:49:05 +0200

    (baby-project-server) Adds in check for channelController context

--- src/test/kotlin/turku/forge/babyproject/AppTests.kt
+++ src/test/kotlin/turku/forge/babyproject/AppTests.kt
@@ -4,16 +4,21 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import turku.forge.babyproject.api.ChannelController
 import turku.forge.babyproject.api.RootApiController
 
 @SpringBootTest
 class AppTests {
 
 	@Autowired
-	lateinit var controller: RootApiController
+	lateinit var rootController: RootApiController
+
+	@Autowired
+	lateinit var channelController: ChannelController
 
 	@Test
-	fun `context loads`() {
-		Assertions.assertThat(controller).isNotNull
+	fun `Validates context loads`() {
+		Assertions.assertThat(rootController).isNotNull
+		Assertions.assertThat(channelController).isNotNull
 	}
 }
